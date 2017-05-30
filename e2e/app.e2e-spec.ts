1934fd4cf093c30a64104dbc4fb6d8aa0e96eb0f
minakshi ghanekar <minakshi_ghanekar@persistent.co.in>
2017-05-30 16:14:35 +0530

    Edited test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,14 +2,14 @@ import { browser, element, by } from 'protractor';
 
 describe('QuickStart E2E Tests', function () {
 
-  let expectedMsg = 'Hello Angular';
+  let expectedMsg = 'Car Assistant';
 
   beforeEach(function () {
     browser.get('');
   });
 
   it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    expect(element(by.css('h3')).getText()).toEqual(expectedMsg);
   });
 
 });
